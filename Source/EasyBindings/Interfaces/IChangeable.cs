874413141e132df6993cfcada932d259d100961b
Ilnaz Khasanov <ilnaz.khasanoff2003@gmail.com>
2023-09-28 14:13:03 +0300

    Small changes

--- Source/EasyBindings/Interfaces/IChangeable.cs
+++ Source/EasyBindings/Interfaces/IChangeable.cs
@@ -4,13 +4,12 @@
 /// Represents an object that can change state
 /// and notifies of its state change by generating
 /// the PropertyChanged event (by implementing INotifyPropertyChanged interface)
-/// with propertyName argument "State"
+/// with propertyName argument "State".
 /// </summary>
 public interface IChangeable
 {
     /// <summary>
-    /// A dummy property needed only to notify that the state of an object
-    /// has changed by generating the PropertyChanged event
+    /// A dummy property needed only to notify that the state of an object has changed by generating the PropertyChanged event.
     /// </summary>
     object? State { get; }
 }
--- Source/EasyBindings/Interfaces/ICommandExecutor.cs
+++ Source/EasyBindings/Interfaces/ICommandExecutor.cs
@@ -1,17 +1,17 @@
 ﻿namespace EasyBindings.Interfaces;
 
 /// <summary>
-/// Provides a mechanism for notifying about the need to execute a command and switch command execution ability
+/// Provides a mechanism for notifying about the need to execute a command and switch command execution ability.
 /// </summary>
 public interface ICommandExecutor
 {
     /// <summary>
-    /// Occurs when there is a need to execute the command
+    /// Occurs when there is a need to execute the command.
     /// </summary>
     event Action? CommandExecutionRequested;
 
     /// <summary>
-    /// A property that allows to switch the ability of an object to execute a command
+    /// A property that allows to switch the ability of an object to execute a command.
     /// </summary>
     bool CanExecuteCommand { get; set; }
 }
