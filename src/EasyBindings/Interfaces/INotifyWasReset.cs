49fa6f46b331fe2ec78847ec20ca629549558925
Ilnaz Khasanov <ilnaz.khasanoff2003@gmail.com>
2023-10-30 22:03:17 +0300

    Some sort of hack

--- src/EasyBindings/Interfaces/INotifyWasReset.cs
+++ src/EasyBindings/Interfaces/INotifyWasReset.cs
@@ -3,12 +3,12 @@
 namespace EasyBindings.Interfaces;
 
 /// <summary>
-/// Notifies clients that a state has resetted.
+/// Notifies clients that a state has reset.
 /// </summary>
-public interface INotifyResetted : INotifyPropertyChanged
+public interface INotifyWasReset : INotifyPropertyChanged
 {
     /// <summary>
     /// The property that will be passed to the <see cref='PropertyChangedEventArgs'/> constructor.
     /// </summary>
-    object? Resetted { get; }
+    object? WasReset { get; }
 }
